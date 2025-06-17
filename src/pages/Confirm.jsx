cd70a5679ff221c7782d38d6eb8d0dbafbce9122
dannamishelle23 <dannamishelle.23@gmail.com>
2025-06-17 14:39:24 -0500

    Update Confirm.jsx

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -9,7 +9,7 @@ export const Confirm = () => {
 
     const verifyToken = async () => {
         try {
-            const url = `${import.meta.env.VITE_BACKEND_URL}/confirmar/${token}`
+            const url = `${import.meta.env.VITE_BACKEND_URL}/confirm/${token}`
             const respuesta = await axios.get(url)
             toast.success(respuesta?.data?.msg)
         } catch (error) {
