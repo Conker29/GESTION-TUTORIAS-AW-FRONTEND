1429a44808220a9b6f981ff4912c79e45ef3448d
dannamishelle23 <dannamishelle.23@gmail.com>
2025-06-17 14:29:59 -0500

    home modificado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,6 @@ import estudiantes from '../assets/estudiantes.webp'
 import logoEPN from '../assets/logoEPN.svg';
 import logoESFOT from '../assets/logoEsfot.png';
 import { Link } from 'react-router';
-import { MdDashboard } from "react-icons/md";
-import { FaRobot } from "react-icons/fa";
-import { FaCommentSms } from "react-icons/fa6";
-import { TbDog } from "react-icons/tb";
-import { GiMedicines } from "react-icons/gi";
 import { FaFacebook } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
@@ -69,84 +64,10 @@ export const Home = () => {
                         BIENVENIDO A LA PLATAFORMA
                     </h1>
                     <p className='text-center text-2xl my-6 font-sans'>
-                        Agenda una cita con tu tutor académico aquí
+                        Agenda una cita con tu tutor académico de acuerdo a su disponibilidad aquí
                     </p>
                 </div>
             </main>
-
-            <section className='container mx-auto px-4'>
-                <div className='container mx-auto relative mt-6'>
-                    <h2 className='font-semibold text-3xl relative z-1 w-50 text-red-900 text-center mx-auto'>MAS INFORMACION</h2>
-                    <div className='text-blue-900 absolute top-1/2 w-full z-0' />
-                </div>
-
-                <div className='my-10 flex flex-col gap-10 items-center sm:flex-row sm:justify-around sm:items-center'>
-                    <div className='px-10 sm:w-1/2'>
-                        <p className='my-4'>Mediante esta plataforma puedes
-                        </p>
-                        <ul className='space-y-4'>
-                            <li>
-                                <MdDashboard className='inline text-2xl mr-2' />Agendar una cita con tu docente de acuerdo a su disponibilidad
-                            </li>
-                            <li>
-                                <FaRobot className='inline text-2xl mr-2' />
-                                Dudas en tiempo real
-                            </li>
-                            <li>
-                                <FaCommentSms className='inline text-2xl mr-2' />
-                                Realtime chat
-                            </li>
-                            <li>
-                                <TbDog className='inline text-2xl mr-2' />
-                                Management Patients
-                            </li>
-                            <li>
-                                <GiMedicines className='inline text-2xl mr-2' />
-                                Management Treatments
-                            </li>
-                        </ul>
-                        <p className='my-4'>And other features that leverage the modern technologies</p>
-                    </div>
-                </div>
-            </section>
-
-
-            <section className='container mx-auto px-4'>
-                <div className='container mx-auto relative mt-6'>
-                    <h2 className='font-semibold text-3xl relative z-1 w-50 text-center mx-auto bg-white'>SERVICIOS</h2>
-                    <div className='text-amber-900 border-2 absolute top-1/2 w-full z-0' />
-                </div>
-
-                <div className='my-10 flex justify-between flex-wrap gap-5'>
-                    <div className="text-center shadow-[0.1rem_0.1rem_1rem_rgba(0,0,0,0.3)] hover:shadow-[0.1rem_0.1rem_1rem_rgba(0,0,0,0.5)] transition-shadow duration-300 relative pt-4 bg-red-50 sm:flex-1">
-                        <TbDog className='inline text-5xl' />
-                        <h4 className="text-xl font-bold py-4 text-amber-700 hover:underline">Management Patients</h4>
-                        <p className="my-4 px-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
-                            similique sint eius consectetur rerum voluptate rem tenetur quisquam veniam quos ad facilis alias
-                            necessitatibus.</p>
-                        <hr className="border-1 border-amber-900 absolute w-full" />
-                    </div>
-
-                    <div className="text-center shadow-[0.1rem_0.1rem_1rem_rgba(0,0,0,0.3)] hover:shadow-[0.1rem_0.1rem_1rem_rgba(0,0,0,0.5)] transition-shadow duration-300 relative pt-4 bg-red-50 sm:flex-1">
-                        <GiMedicines className='inline text-5xl' />
-                        <h4 className="text-xl font-bold py-4 text-amber-700 hover:underline">Management Treatments</h4>
-                        <p className="my-4 px-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
-                            similique sint eius consectetur rerum voluptate rem tenetur quisquam veniam quos ad facilis alias
-                            necessitatibus.</p>
-                        <hr className="border-1 border-amber-900 absolute w-full" />
-                    </div>
-
-                    <div className="text-center shadow-[0.1rem_0.1rem_1rem_rgba(0,0,0,0.3)] hover:shadow-[0.1rem_0.1rem_1rem_rgba(0,0,0,0.5)] transition-shadow duration-300 relative pt-4 sm:flex-1">
-                        <FaCommentSms className='inline text-5xl' />
-                        <h4 className="text-xl font-bold py-4 text-amber-700 hover:underline">Realtime Chat</h4>
-                        <p className="my-4 px-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
-                            similique sint eius consectetur rerum voluptate rem tenetur quisquam veniam quos ad facilis alias
-                            necessitatibus.</p>
-                        <hr className="border-1 border-amber-900 absolute w-full" />
-                    </div>
-                </div>
-            </section>
-
 
             <footer className='text-center bg-gray-50 p-6 sm:px-20 sm:py-10 mt-20 rounded-tr-3xl rounded-tl-3xl space-y-8'>
 
@@ -154,7 +75,6 @@ export const Home = () => {
                     <div className='text-left'>
                     <p className='text-3xl font-extrabold text-amber-800'>Contacto</p>
                         <p className='font-bold my-2'>Email: tutorias.esfot@gmail.com</p>
-                        <p className='font-bold'>Para más información acercarse a la casona de la ESFOT</p>
                     </div>
                     <ul className='flex gap-4'>
                         <li><FaFacebook className='text-2xl' /></li>
